54ecd050131134f67b9e0df53c18bccd29a24cd7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 19:17:55 +0200

    Check if the input is number or not function is implemented

--- Validation/formValidation.php
+++ Validation/formValidation.php
@@ -1,8 +1,16 @@
 <?php 
 
-function testTheInput($input) {
+// prevent Cross-Site Scripting attacks
+function toBeSafe($input) {
     $input = trim($input);
     $input = stripslashes($input);
     $input = htmlspecialchars($input);
     return $input;
+}
+
+function isNumber($data){
+    if (!preg_match ("/^[0-9]*$/", $data) ){  
+        return false;  
+    }
+    return true;
 }
