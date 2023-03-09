7b9ee6d073e4e0e8047ae52f273942afc69d6c9d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-10 01:49:27 +0200

    Authorization middleware is implemented

--- Middleware/authorization.php
+++ Middleware/authorization.php
@@ -7,3 +7,26 @@ header('Content-Type: application/json; charset=utf-8');
 include("../database.php");
 include("../Authentication/jwt.php");
 include("../Validation/formValidation.php");
+
+if(isset(apache_request_headers()["Authorization"])){
+    $arr = explode(" ", apache_request_headers()["Authorization"]);
+    try {
+        $decoded = HandleJWT::decode_JWT_token($arr[1]);
+        echo "Bannananana!";
+    }catch (SignatureInvalidException $e) {
+        echo json_encode([
+            "error" => "unauthorized action"
+        ]);
+        die();
+    }catch (ExpiredException $e) {
+        echo json_encode([
+            "error" => "token expired"
+        ]);
+        die();
+    }catch(Exception $e){
+        echo json_encode([
+            "error" => "unauthorized action"
+        ]);
+        die();
+    }
+}
