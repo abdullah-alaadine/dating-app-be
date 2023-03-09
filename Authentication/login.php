2c60deb6a6014f8609c6530a0c5907a84103635f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 23:32:53 +0200

    fetching data after submiting logging in fields is done

--- Authentication/login.php
+++ Authentication/login.php
@@ -11,6 +11,22 @@ include("../Validation/formValidation.php");
 if(isset($_POST["email"]) && isset($_POST["password"])){
     $email = to_be_safe($_POST["email"]);
     $password = to_be_safe($_POST["password"]);
+    $query = "SELECT `email` FROM `users` WHERE `email` = '".$email."'";
+    $result = $connection->query($query);
+    if ($result->num_rows > 0){
+        $data = $result->fetch_assoc();
+        if(!password_verify($password, $data["password"])){
+            echo json_encode([
+                "error" => "incorrect data"
+            ]);
+            die();
+        }
+    } else {
+        echo json_encode([
+            "error" => "incorrect data"
+        ]);
+        die();
+    }
 }else{
     echo json_encode([
         "error" => "Some uninserted fields are required!"
