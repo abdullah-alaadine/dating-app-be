1f91c14adbab53130ff53009a0c2d37e8a654b7d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 18:22:25 +0200

    error fixed in HandleJWT class, self keyword is used instead of $this keyword

--- Authentication/jwt.php
+++ Authentication/jwt.php
@@ -6,12 +6,15 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 class HandleJWT {
-    private $key = "akf23w3r34rQ#O891-rh4";
-    private $algorithm = "HS256";
+
+    private static $key = "akf23w3r34rQ#O891-rh4";
+    private static $algorithm = "HS256";
+
     public static function get_JWT_token($payload){
-        return JWT::encode($payload, this->$key, this->$algorithm);
+        return JWT::encode($payload, self::$key, self::$algorithm);
     }
+
     public static function decode_JWT_token($token){
-        return JWT::decode($token, new Key(this->key, this->algorithm));
+        return JWT::decode($token, new Key(self::$key, self::$algorithm));
     }
 }
