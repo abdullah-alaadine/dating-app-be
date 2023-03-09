0f46da653e76324138ad7da417722d34a27cefd3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 20:07:37 +0200

    firstName validation is signup API is done!

--- Authentication/signup.php
+++ Authentication/signup.php
@@ -18,10 +18,15 @@ if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["firstNam
         echo json_encode([
             "error" => "unvalid email"
         ]);
-        die();
+        die(); // equivalent to exit()
     }else if(!is_strong_password($password)){
         echo json_encode([
             "error" => "the password isn't strong enough! please make sure to follow the following criteria when choosing a strong pass: \n\n 1- The password should contain at least one capital letter.\n\n 2- The password should contain at least one special character. \n\n 3- The password should contain at least one number. \n\n 4- The password should consist of 8 characters at least."
+        ]);
+        die();
+    }else if(!valid_name($firstName)){
+        echo json_encode([
+            "error" => "unvalid name"
         ]);
         die();
     }
