68e02914d5bafd326f7d588a61e6a3a230f8cc2b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 23:14:44 +0200

    login API is protected form Cross-Site Scripting attacks

--- Authentication/login.php
+++ Authentication/login.php
@@ -9,7 +9,8 @@ include("../Authentication/jwt.php");
 include("../Validation/formValidation.php");
 
 if(isset($_POST["email"]) && isset($_POST["password"])){
-
+    $email = to_be_safe($_POST["email"]);
+    $password = to_be_safe($_POST["password"]);
 }else{
     echo json_encode([
         "error" => "Some uninserted fields are required!"
