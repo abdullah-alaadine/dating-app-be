bc2f99cc53f1f1b82e41f006bfa6f83b21d4a2a3
Muhammad Ali <125280380+masafiedin@users.noreply.github.com>
2023-03-09 09:56:28 +0200

    created users table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,16 @@
 create database datingappdb;
 use datingappdb;
+
+
+CREATE TABLE users (
+    userID int(11) NOT NULL AUTO_INCREMENT,
+    email varchar(128) not null,
+    password text(512) not null,
+    lastname varchar(255),
+    fistname varchar(255) NOT NUlL,
+    age int,
+    country varchar(64) not null,
+    gender_id int(16) not null,
+    PRIMARY KEY (userid),
+    foreign key(gender_id) References genders(gender_id)
+);
