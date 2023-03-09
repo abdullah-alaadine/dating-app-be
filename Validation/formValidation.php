85edd0811af6454d8ff2b92fbe2951d02628b855
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-09 19:25:17 +0200

    Check if the input is a valid email or not function is implemented

--- Validation/formValidation.php
+++ Validation/formValidation.php
@@ -21,3 +21,11 @@ function valid_name($data){
     }
     return true;
 }
+
+function valid_email($email){
+    $pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
+    if (!preg_match ($pattern, $email) ){  
+        return false; 
+    }
+    return true;
+}
